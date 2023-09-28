568bca80e65ec3e9b21b909dc565a2a4f7a3f3ca
Alexei196 <dndy778@gmail.com>
2023-09-27 17:23:08 -0700

    added pretrained keras ocr

--- readBarcodes.py
+++ readBarcodes.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import os, sys
 from cv2 import barcode
 
+import keras_ocr
+
 def fetch_images(path: str):
     pathList = [] 
     with open(path, "r") as file:
@@ -20,6 +22,16 @@ def get_roi_of_image(image, points):
     # cv.waitKey(0)
     return image[y:y+h, x:x+w]
 
+def detect_text_in_image(path, pipeline):
+    image = keras_ocr.tools.read(path)
+    # data is of type (word, boundbox) where boundingbox is 4 connected segments
+    data = pipeline.recognize([image])[0]
+    print(f"The word is {data[0]}")
+    image = cv.line(image, data[1][0], data[1][1])
+    image = cv.line(image, data[1][1], data[1][2])
+    image = cv.line(image, data[1][2], data[1][3])
+    image = cv.line(image, data[1][3], data[1][0])
+
 def main() :
     image_paths = fetch_images("barcodes.csv")
 
@@ -27,14 +39,15 @@ def main() :
         os.mkdir("output")
     
     detector = barcode.BarcodeDetector()
+    pipeline = keras_ocr.pipeline.Pipeline()
 
     for id, path in image_paths:
-        print(id, f"\"{path}\"")
         image = cv.imread(path)
         value, points = detector.detectMulti((image))
         print(f"I report {value} for {path}!")
         if value is True:
-            image = get_roi_of_image(image, points)
+            image = get_roi_of_image(image, points)  
+            detect_text_in_image(path, pipeline)
             
 
 if __name__ == "__main__":
